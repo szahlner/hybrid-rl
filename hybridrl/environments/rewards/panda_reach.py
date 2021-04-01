e16402c58afeb6e221ec333b09fc829ff6a6b072
szahlner <stefan.zahlner@gmx.at>
2021-04-01 16:45:14 +0200

    update reward functions

--- hybridrl/environments/rewards/panda_reach.py
+++ hybridrl/environments/rewards/panda_reach.py
@@ -5,7 +5,7 @@ class PandaReachV0Reward:
     """PandaReach-v0 reward"""
 
     def __call__(self, obs, actions, obs_next):
-        achieved_goal = obs[:, -6:-3]
-        desired_goal = obs[:, -3:]
+        achieved_goal = obs_next[:, -6:-3]
+        desired_goal = obs_next[:, -3:]
 
         return -np.linalg.norm(achieved_goal - desired_goal, axis=-1)
--- hybridrl/environments/rewards/shadow_hand_reach.py
+++ hybridrl/environments/rewards/shadow_hand_reach.py
@@ -5,7 +5,7 @@ class ShadowHandReachV0Reward:
     """ShadowHandReach-v0 reward"""
 
     def __call__(self, obs, actions, obs_next):
-        achieved_goal = obs[:, -30:-15]
-        desired_goal = obs[:, -15:]
+        achieved_goal = obs_next[:, -30:-15]
+        desired_goal = obs_next[:, -15:]
 
         return -np.linalg.norm(achieved_goal - desired_goal, axis=-1)

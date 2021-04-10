c92a08b3fd78c62c8ffcf8c5e5b7e3af38e2df9d
szahlner <stefan.zahlner@gmx.at>
2021-04-10 15:23:11 +0200

    update default polyak value

--- hybridrl/validation/ddpg_schema.py
+++ hybridrl/validation/ddpg_schema.py
@@ -10,7 +10,7 @@ ddpg_schema = {
     Optional('noise_eps_decay', default=0.001): And(float, lambda n: 0 <= n),
     Optional('batch_size', default=10): And(int, lambda n: n > 0),
     Optional('gamma', default=0.98): And(float, lambda n: 0 < n <= 1),
-    Optional('polyak', default=0.95): And(float, lambda n: 0 <= n <= 1),
+    Optional('polyak', default=0.005): And(float, lambda n: 0 <= n <= 1),
     Optional('random_exploration', default=0.3): And(float, lambda n: 0 <= n <= 1)
 }
 

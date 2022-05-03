e4343bcb30e1ac5bc8f48e3762eea246c963bf7d
szahlner <stefan.zahlner@gmx.at>
2022-05-03 12:32:17 +0200

    update actor and critic nets to fit locomotive needs

--- policy/ddpg.py
+++ policy/ddpg.py
@@ -34,13 +34,13 @@ class Actor(nn.Module):
 
         self.fc1 = nn.Linear(env_params["obs"] + env_params["goal"], 256)
         self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 256)
+        # self.fc3 = nn.Linear(256, 256)
         self.action_out = nn.Linear(256, env_params["action"])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
+        # x = torch.relu(self.fc3(x))
         actions = self.max_action * torch.tanh(self.action_out(x))
 
         return actions
@@ -60,14 +60,14 @@ class Critic(nn.Module):
 
         self.fc1 = nn.Linear(env_params["obs"] + env_params["goal"] + env_params["action"], 256)
         self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 256)
+        # self.fc3 = nn.Linear(256, 256)
         self.q_out = nn.Linear(256, 1)
 
     def forward(self, x: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
         x = torch.cat([x, actions / self.max_action], dim=1)
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
+        # x = torch.relu(self.fc3(x))
         q_value = self.q_out(x)
 
         return q_value

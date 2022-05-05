e3de24b890d49fcd70f095c9fabcae1b6d4b7c3b
szahlner <stefan.zahlner@gmx.at>
2022-05-05 14:06:31 +0200

    update run_her scripts

--- scripts/run_her_sac_experiments.sh
+++ scripts/run_her_sac_experiments.sh
@@ -2,17 +2,17 @@
 cd "/home/hybrid-rl/hybrid-rl"
 
 # FetchPush-v1
-python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-dim-chunk 19
-python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-dim-chunk 19
-python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-dim-chunk 19
+python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-dim-chunk 30
+python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-dim-chunk 30
+python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-dim-chunk 30
 
-python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19
-python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19
-python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19
+python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
+python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
+python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
 
-# python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 19
-# python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 19
-# python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 19
+# python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
+# python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
+# python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
 
 # ShadowHandBlock-v1
 python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --alpah 0.01

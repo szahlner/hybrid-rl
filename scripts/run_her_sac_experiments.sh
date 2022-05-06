daf704c99923a2798f50db678efe8489d5be226b
szahlner <stefan.zahlner@gmx.at>
2022-05-06 12:04:23 +0200

    update her_sac_experiments

--- scripts/run_her_sac_experiments.sh
+++ scripts/run_her_sac_experiments.sh
@@ -6,9 +6,9 @@ python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cy
 python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40
 python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40
 
-python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
-python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
-python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
+python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19 --model-training-freq 1000
+python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19 --model-training-freq 1000
+python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19 --model-training-freq 1000
 
 # python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
 # python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
@@ -19,9 +19,9 @@ python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750
 python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --alpah 0.01
 python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 3750 --n-cycles 40 --alpah 0.01
 
-python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based
-python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based
-python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based
+python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 1000
+python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 1000
+python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 1000
 
 # python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based --model-type stochastic
 # python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --alpah 0.01 --model-based --model-type stochastic

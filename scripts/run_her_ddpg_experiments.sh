d4ca20bd19551dbd8dff95e8fc4193be244fb828
szahlner <stefan.zahlner@gmx.at>
2022-05-06 11:49:55 +0200

    update her_ddpg_experiments

--- scripts/run_her_ddpg_experiments.sh
+++ scripts/run_her_ddpg_experiments.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd "/home/hybrid-rl/hybrid-rl"
 
-# FetchPush-v1
-python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40
-python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40
-python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40
+# FetchPush-v1 DONE
+# python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40
+# python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40
+# python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40
 
-python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
-python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
-python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 30
+python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19 --model-training-freq 1000
+python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19 --model-training-freq 1000
+python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 400 --n-cycles 40 --model-based --model-dim-chunk 19 --model-training-freq 1000
 
 # python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
 # python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 400 --n-cycles 40 --model-based --model-type stochastic --model-dim-chunk 30
@@ -19,9 +19,9 @@ python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 375
 python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40
 python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 3750 --n-cycles 40
 
-python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --model-based
-python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --model-based
-python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 3750 --n-cycles 40 --model-based
+python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --model-based --model-training-freq 1000
+python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --model-based --model-training-freq 1000
+python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 3750 --n-cycles 40 --model-based --model-training-freq 1000
 
 # python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 3750 --n-cycles 40 --model-based --model-type stochastic
 # python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 3750 --n-cycles 40 --model-based --model-type stochastic

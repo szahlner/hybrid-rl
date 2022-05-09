1aaa0a71fe28f9480e58741e94074b7534634c06
szahlner <stefan.zahlner@gmx.at>
2022-05-09 12:46:08 +0200

    update run_sac Ant-v2 and Humanoid-v2

--- scripts/run_sac_experiments.sh
+++ scripts/run_sac_experiments.sh
@@ -39,3 +39,29 @@ python train_sac.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 -
 python train_sac.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.95
 python train_sac.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.95
 python train_sac.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.95
+
+# SAC Ant-v2
+python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+
+python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
+python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
+python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
+
+# python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+# python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+# python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+
+# SAC Humanoid-v2
+python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
+python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
+python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
+
+python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
+python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
+python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
+
+# python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+# python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+# python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based --model-type stochastic --model-stochastic-percentage 0.83

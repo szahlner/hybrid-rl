4461e3992be71779234b7cdfcabf2347b4a1bfaa
szahlner <stefan.zahlner@gmx.at>
2022-05-13 08:57:19 +0200

    update ddpg and sac run_experiments

--- scripts/run_ddpg_experiments.sh
+++ scripts/run_ddpg_experiments.sh
@@ -6,9 +6,9 @@ cd "/home/hybrid-rl/hybrid-rl"
 # python train_ddpg.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000
 # python train_ddpg.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000
 
-# python train_ddpg.py --env-name Hopper-v2 --seed 123 --max-timesteps 125000 --model-based
-# python train_ddpg.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --model-based
-# python train_ddpg.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --model-based
+python train_ddpg.py --env-name Hopper-v2 --seed 123 --max-timesteps 125000 --model-based
+python train_ddpg.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --model-based
+python train_ddpg.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --model-based
 
 python train_ddpg.py --env-name Hopper-v2 --seed 123 --max-timesteps 125000 --model-based --model-type stochastic --model-stochastic-percentage 0.90
 python train_ddpg.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --model-based --model-type stochastic --model-stochastic-percentage 0.90
@@ -19,9 +19,9 @@ python train_ddpg.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --
 # python train_ddpg.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000
 # python train_ddpg.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000
 
-# python train_ddpg.py --env-name Walker2d-v2 --seed 123 --max-timesteps 300000 --model-based
-# python train_ddpg.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --model-based
-# python train_ddpg.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 --model-based
+python train_ddpg.py --env-name Walker2d-v2 --seed 123 --max-timesteps 300000 --model-based
+python train_ddpg.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --model-based
+python train_ddpg.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 --model-based
 
 python train_ddpg.py --env-name Walker2d-v2 --seed 123 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.60
 python train_ddpg.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.60
@@ -32,35 +32,35 @@ python train_ddpg.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 
 # python train_ddpg.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000
 # python train_ddpg.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000
 
-# python train_ddpg.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --model-based
-# python train_ddpg.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --model-based
-# python train_ddpg.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --model-based
+python train_ddpg.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --model-based
+python train_ddpg.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --model-based
+python train_ddpg.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --model-based
 
 python train_ddpg.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
 python train_ddpg.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
 python train_ddpg.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
 
 # DDPG Ant-v2
-python train_ddpg.py --env-name Ant-v2 --seed 123 --max-timesteps 300000
-python train_ddpg.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000
-python train_ddpg.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000
+# python train_ddpg.py --env-name Ant-v2 --seed 123 --max-timesteps 300000
+# python train_ddpg.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000
+# python train_ddpg.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000
 
 python train_ddpg.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --model-based
 python train_ddpg.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --model-based
 python train_ddpg.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --model-based
 
-# python train_ddpg.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
-# python train_ddpg.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
-# python train_ddpg.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+python train_ddpg.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.10
+python train_ddpg.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.10
+python train_ddpg.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.10
 
 # DDPG Humanoid-v2
-python train_ddpg.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000
-python train_ddpg.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000
-python train_ddpg.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000
+# python train_ddpg.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000
+# python train_ddpg.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000
+# python train_ddpg.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000
 
-python train_ddpg.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --model-based
-python train_ddpg.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --model-based
-python train_ddpg.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --model-based
+# python train_ddpg.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --model-based
+# python train_ddpg.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --model-based
+# python train_ddpg.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --model-based
 
 # python train_ddpg.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
 # python train_ddpg.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --model-based --model-type stochastic --model-stochastic-percentage 0.83
--- scripts/run_sac_experiments.sh
+++ scripts/run_sac_experiments.sh
@@ -6,9 +6,9 @@ cd "/home/hybrid-rl/hybrid-rl"
 # python train_sac.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1
 # python train_sac.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1
 
-# python train_sac.py --env-name Hopper-v2 --seed 123 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based
-# python train_sac.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based
-# python train_sac.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based
+python train_sac.py --env-name Hopper-v2 --seed 123 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based
+python train_sac.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based
+python train_sac.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based
 
 python train_sac.py --env-name Hopper-v2 --seed 123 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based --model-type stochastic --model-stochastic-percentage 0.95
 python train_sac.py --env-name Hopper-v2 --seed 1234 --max-timesteps 125000 --automatic-entropy-tuning --target-entropy -1 --model-based --model-type stochastic --model-stochastic-percentage 0.95
@@ -19,9 +19,9 @@ python train_sac.py --env-name Hopper-v2 --seed 12345 --max-timesteps 125000 --a
 # python train_sac.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3
 # python train_sac.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3
 
-# python train_sac.py --env-name Walker2d-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based
-# python train_sac.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based
-# python train_sac.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based
+python train_sac.py --env-name Walker2d-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based
+python train_sac.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based
+python train_sac.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based
 
 python train_sac.py --env-name Walker2d-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.60
 python train_sac.py --env-name Walker2d-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.60
@@ -32,35 +32,35 @@ python train_sac.py --env-name Walker2d-v2 --seed 12345 --max-timesteps 300000 -
 # python train_sac.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3
 # python train_sac.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3
 
-# python train_sac.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based
-# python train_sac.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based
-# python train_sac.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based
+python train_sac.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based
+python train_sac.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based
+python train_sac.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based
 
 python train_sac.py --env-name HalfCheetah-v2 --seed 123 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.95
 python train_sac.py --env-name HalfCheetah-v2 --seed 1234 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.95
 python train_sac.py --env-name HalfCheetah-v2 --seed 12345 --max-timesteps 400000 --automatic-entropy-tuning --target-entropy -3 --model-based --model-type stochastic --model-stochastic-percentage 0.95
 
 # SAC Ant-v2
-python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
-python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
-python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+# python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+# python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+# python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
 
 python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
 python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
 python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
 
-# python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.83
-# python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.83
-# python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.83
+python train_sac.py --env-name Ant-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.10
+python train_sac.py --env-name Ant-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.10
+python train_sac.py --env-name Ant-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based --model-type stochastic --model-stochastic-percentage 0.10
 
 # SAC Humanoid-v2
-python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
-python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
-python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
+# python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
+# python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
+# python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2
 
-python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
-python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
-python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
+# python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
+# python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
+# python train_sac.py --env-name Humanoid-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based
 
 # python train_sac.py --env-name Humanoid-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based --model-type stochastic --model-stochastic-percentage 0.83
 # python train_sac.py --env-name Humanoid-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -2 --model-based --model-type stochastic --model-stochastic-percentage 0.83

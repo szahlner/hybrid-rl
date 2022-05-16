91cc71378dcea1206d3b66116b7984283506b0d7
szahlner <stefan.zahlner@gmx.at>
2022-05-16 14:28:07 +0200

    rearrange run_her scripts

--- scripts/run_her_sac_experiments.sh
+++ scripts/run_her_sac_experiments.sh
@@ -29,15 +29,15 @@ cd "/home/hybrid-rl/hybrid-rl"
 
 # FetchPush-v1 - MPI version
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 50 --n-cycles 40
-mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40
-mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40
-
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
-
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 123 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+
+mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40
+mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+
+mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40
+mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
 # ShadowHandBlock-v1 - MPI version
@@ -46,9 +46,10 @@ mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1
 # mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01
 
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
-mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
-mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
-
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
+
+mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
+
+mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
 mpirun -np 8 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80

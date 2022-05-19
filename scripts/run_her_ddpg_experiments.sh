b04c6b51048fcfdab1df29883ade0219b4a4fa3b
szahlner <stefan.zahlner@gmx.at>
2022-05-19 13:54:21 +0200

    add FetchPushTruncated-v1 in ddpg and sac run_scripts

--- scripts/run_her_ddpg_experiments.sh
+++ scripts/run_her_ddpg_experiments.sh
@@ -28,27 +28,41 @@ cd "/home/hybrid-rl/hybrid-rl"
 # python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --model-based --model-type stochastic
 
 # FetchPush-v1 - MPI version
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 50 --n-cycles 40
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 250 --n-cycles 40
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 250 --n-cycles 40
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 50 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 250 --n-cycles 40
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPush-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+
+# FetchPushTruncated-v1 - MPI version
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+
 
 # ShadowHandBlock-v1 - MPI version
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
 
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
 
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
-mpirun -np 8 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200
+# mpirun -np 1 --allow-run-as-root python train_her_ddpg.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpah 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80

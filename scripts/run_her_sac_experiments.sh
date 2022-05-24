1b0a135ed3c8bd963457eeb7a9b348641dec5d8d
szahlner <stefan.zahlner@gmx.at>
2022-05-24 21:15:06 +0200

    update run_her_sac

--- scripts/run_her_sac_experiments.sh
+++ scripts/run_her_sac_experiments.sh
@@ -41,17 +41,17 @@ cd "/home/hybrid-rl/hybrid-rl"
 # mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPush-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
 # FetchPushTruncated-v1 - MPI version
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 123 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 1234 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
-mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15
+# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTruncated-v1 --seed 12345 --n-epochs 250 --n-cycles 40 --model-based --model-dim-chunk 15 --model-type stochastic --model-stochastic-percentage 0.90
 
 
 # ShadowHandBlock-v1 - MPI version
@@ -59,7 +59,7 @@ mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name FetchPushTru
 # mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpha 0.01
 # mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 12345 --n-epochs 1000 --n-cycles 40 --alpha 0.01
 
-# mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpha 0.01 --model-based --model-training-freq 200
+mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpha 0.01 --model-based --model-max-rollout-timesteps 1
 # mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 123 --n-epochs 1000 --n-cycles 40 --alpha 0.01 --model-based --model-training-freq 200 --model-type stochastic --model-stochastic-percentage 0.80
 
 # mpirun -np 1 --allow-run-as-root python train_her_sac.py --env-name ShadowHandBlock-v1 --seed 1234 --n-epochs 1000 --n-cycles 40 --alpha 0.01 --model-based --model-training-freq 200

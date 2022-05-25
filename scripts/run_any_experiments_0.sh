ff8fd2b07e93dd76e59ad04ab317285870f1e15f
szahlner <stefan.zahlner@gmx.at>
2022-05-25 20:10:49 +0200

    update any_experiments_0

--- scripts/run_any_experiments_0.sh
+++ scripts/run_any_experiments_0.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 cd "/home/hybrid-rl/hybrid-rl"
+
+# DDPG AntTruncated-v2
+python train_ddpg.py --env-name AntTruncated-v2 --seed 123 --max-timesteps 300000
+python train_ddpg.py --env-name AntTruncated-v2 --seed 1234 --max-timesteps 300000
+python train_ddpg.py --env-name AntTruncated-v2 --seed 12345 --max-timesteps 300000
+
+python train_ddpg.py --env-name AntTruncated-v2 --seed 123 --max-timesteps 300000 --n-batches 2
+python train_ddpg.py --env-name AntTruncated-v2 --seed 1234 --max-timesteps 300000 --n-batches 2
+python train_ddpg.py --env-name AntTruncated-v2 --seed 12345 --max-timesteps 300000 --n-batches 2
+
+python train_ddpg.py --env-name AntTruncated-v2 --seed 123 --max-timesteps 300000 --model-based
+python train_ddpg.py --env-name AntTruncated-v2 --seed 1234 --max-timesteps 300000 --model-based
+python train_ddpg.py --env-name AntTruncated-v2 --seed 12345 --max-timesteps 300000 --model-based
+
+# SAC AntTruncated-v2
+python train_sac.py --env-name AntTruncated-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+python train_sac.py --env-name AntTruncated-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+python train_sac.py --env-name AntTruncated-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4
+
+python train_sac.py --env-name AntTruncated-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --n-batches 2
+python train_sac.py --env-name AntTruncated-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --n-batches 2
+python train_sac.py --env-name AntTruncated-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --n-batches 2
+
+python train_sac.py --env-name AntTruncated-v2 --seed 123 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
+python train_sac.py --env-name AntTruncated-v2 --seed 1234 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
+python train_sac.py --env-name AntTruncated-v2 --seed 12345 --max-timesteps 300000 --automatic-entropy-tuning --target-entropy -4 --model-based
